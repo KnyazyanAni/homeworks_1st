e132edad5b86acb1a6c8bdb2ff965c99124783f4
Hayk Aghajanyan <aghajanyan.haykv@gmail.com>
2021-12-16 20:37:36 +0400

    checked

--- home_work_9__9.12/src/App.js
+++ home_work_9__9.12/src/App.js
@@ -1,43 +1,40 @@
-import { Component } from 'react'
+import {Component} from 'react'
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataState: null,
-      res: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataState: null,
+            res: [],
+            searchValue: ''
+        }
     }
-  }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(response => response.json())
-      .then(data => this.setState({ dataState: data }))
-      .catch(err => console.error(err));
-  }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(data => this.setState({dataState: data}))
+            .catch(err => console.error(err));
+    }
 
-  foo = () => {
-    this.state.dataState.map(item => {
-      if (document.querySelector('.searchInput').value === item.title) {
-        this.setState({ res: [...this.state.res, item] })
-      }
-    })
-  }
+    handleSearch = (e) => {
+        this.setState({searchValue: e.target.value})
+    }
 
-  render() {
-    console.log(this.state.dataState);
-    console.log(this.state.res);
-    return (
-      <>
-        <input className='searchInput'></input>
-        <button className='searchButton' onClick={this.foo}>click</button>
-        <div className='resultDiv'>{
-          this.state.res.map(item => <div key={item.id}>UserId:{item.userId}, Id:{item.id}, Title:{item.title}, Completed:{item.completed}</div>)
-        }</div>
-      </>
-    )
-  }
+    render() {
+        const {res, searchValue} = this.state
+        return (
+            <>
+                <input value={searchValue} onChange={this.handleSearch} className='searchInput'/>
+                <button className='searchButton'>click</button>
+                <div className='resultDiv'>{
+                    res.map(item => <div key={item.id}>UserId:{item.userId}, Id:{item.id}, Title:{item.title},
+                        Completed:{item.completed}</div>)
+                }</div>
+            </>
+        )
+    }
 }
 
 export default App;
