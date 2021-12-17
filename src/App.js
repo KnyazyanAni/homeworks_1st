089c3ca0065cbbecf93fb11ee0205b6804a4fab2
ArshNaira <arshnaira@mail.ru>
2021-12-17 12:07:08 +0400

    Arsh_Naira

--- src/App.js
+++ src/App.js
@@ -4,28 +4,55 @@ class App extends Component {
   constructor(props){
   super(props);
 this.state={
-  toDoArr:[]
-}
+  data:[],
+  isChange:false,
+           }
+ this.handleFilterText=this.handleFilterText.bind(this);
 
   }
 
-  
-addToDoes(){
-fetch('https://jsonplaceholder.typicode.com/todos')
-.then(res=>res.json())
-.then(result=>{
-  
-})
-
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(res => res.json())
+    .then((res) => {
+      this.setState({ data: res })
+    })
 }
 
 
+
+
+handleFilterText=(ev)=>{
+  let filterValue=ev.target.value;
+this.setState({isChange:!this.state.isChange})
+   this.state.data.map((item)=>{
+     
+     item.isHidden=!item.title.includes(filterValue);
+     return item;
+   }
+
+   )
+ 
+
+}
+
   render(){
-    return (
-      <div>
-      <input ></input>
-        111111
-      </div>
+    return (<>
+   <input className="inputTxt" onChange={this.handleFilterText} ></input>
+      <div className="container">
+      {
+        this.state.data.map(item=>{
+          return !item.isHidden ? <div key={item.id}
+                 id={item.id}
+                 className="title"
+                 >
+              {item.title}
+            </div> : null
+          
+        })
+      }
+    </div>
+    </>
     )
   }
 }
